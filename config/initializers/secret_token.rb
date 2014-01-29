81f09ec2cc7a56ea1e3d038da2411cd0695136b8
Billy Ceskavich <bceskavich@gmail.com>
2014-01-29 11:22:10 -0500

    Uncached certain files.

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Compas::Application.config.secret_token = 'b86ce64035ce3696bb203b89f044c9ffed96a205b0e05caa3f6a991108598ea0da09b10075ee6a1c6a14aaa294e98f8970a58474e4800b4471b5920a12a71be5'
