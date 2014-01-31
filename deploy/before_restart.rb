59f9934e8d319d5a412f57cbf12f2dbbb2da4a7f
Billy Ceskavich <bceskavich@gmail.com>
2014-01-31 00:09:53 -0500

    App name modified to match AWS stack.

--- deploy/before_restart.rb
+++ deploy/before_restart.rb
@@ -1,1 +1,1 @@
-run "cd /srv/www/compas-beta/current && /usr/local/bin/bundle exec rake assets:precompile"	
+run "cd /srv/www/compas_beta/current && /usr/local/bin/bundle exec rake assets:precompile"	
