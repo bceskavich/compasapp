1b994f5bfdd9dac51bbdef330987253fa5081524
Billy Ceskavich <bceskavich@gmail.com>
2014-02-01 13:06:41 -0500

    AWS deploy with seed.

--- deploy/before_restart.rb
+++ deploy/before_restart.rb
@@ -1,2 +1,3 @@
 run "cd /srv/www/compas_beta/current && /usr/local/bin/bundle exec rake assets:precompile"
+run "cd /srv/www/compas_beta/current && /usr/local/bin/bundle exec rake db:migrate"	
 run "cd /srv/www/compas_beta/current && /usr/local/bin/bundle exec rake db:seed"	
